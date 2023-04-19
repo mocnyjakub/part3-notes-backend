cb8f34595fc04bf94dc1360e728599d4f5291ec2
mocnyjakub <mocnyjakub@gmail.com>
2023-04-19 20:44:12 +0200

    Add mongoDB connection

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+const Person = require('./models/person');
 
 morgan.token('body', function getBody(req) {
   return req.method === 'POST' ? JSON.stringify(req.body) : '';
@@ -39,32 +18,31 @@ app.use(express.json());
 app.use(express.static('build'));
 app.use(morgan(morganConfig));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello World!</h1>');
+// });
 
-app.get('/info', (req, res) => {
-  const numberOfPersons = persons.length;
-  const pageContent = `
-  <span>Phonebook has info for ${numberOfPersons} people</span>
-  <br />
-  <span>${new Date()}</span>
-  `;
-  res.send(pageContent);
-});
+// app.get('/info', (req, res) => {
+//   const numberOfPersons = persons.length;
+//   const pageContent = `
+//   <span>Phonebook has info for ${numberOfPersons} people</span>
+//   <br />
+//   <span>${new Date()}</span>
+//   `;
+//   res.send(pageContent);
+// });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  const id = req.params.id;
+  Person.findById(id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.post('/api/persons', (req, res) => {
@@ -76,33 +54,27 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique',
-    });
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 1000000),
+  person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons.push(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const movieToDelete = persons.find((person) => person.id === id);
-  if (!movieToDelete) {
-    return res.status(404).json({
-      error: 'person not found',
-    });
-  }
-  persons.splice(persons.indexOf(movieToDelete), 1);
-  res.status(204).end();
-});
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id);
+//   const movieToDelete = persons.find((person) => person.id === id);
+//   if (!movieToDelete) {
+//     return res.status(404).json({
+//       error: 'person not found',
+//     });
+//   }
+//   persons.splice(persons.indexOf(movieToDelete), 1);
+//   res.status(204).end();
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
