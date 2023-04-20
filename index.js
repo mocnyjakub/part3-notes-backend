a2c693d10cd1228572566200d2eec229a07dae81
mocnyjakub <mocnyjakub@gmail.com>
2023-04-20 12:02:13 +0200

    Add eslint

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ app.use(express.json());
 app.use(express.static('build'));
 app.use(morgan(morganConfig));
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello World!</h1>');
-// });
-
-// app.get('/info', (req, res) => {
-//   const numberOfPersons = persons.length;
-//   const pageContent = `
-//   <span>Phonebook has info for ${numberOfPersons} people</span>
-//   <br />
-//   <span>${new Date()}</span>
-//   `;
-//   res.send(pageContent);
-// });
+app.get('/info', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      const numberOfPersons = persons.length;
+      const pageContent = `
+    <span>Phonebook has info for ${numberOfPersons} people</span>
+    <br />
+    <span>${new Date()}</span>
+    `;
+      res.send(pageContent);
+    })
+    .catch((error) => next(error));
+});
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
@@ -38,14 +38,22 @@ app.get('/api/persons', (req, res) => {
   });
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then((person) => {
-    res.json(person);
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -54,27 +62,74 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  person = new Person({
+  const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-// app.delete('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id);
-//   const movieToDelete = persons.find((person) => person.id === id);
-//   if (!movieToDelete) {
-//     return res.status(404).json({
-//       error: 'person not found',
-//     });
-//   }
-//   persons.splice(persons.indexOf(movieToDelete), 1);
-//   res.status(204).end();
-// });
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      if (result) {
+        res.status(204).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id;
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+  Person.findByIdAndUpdate(id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        res.json(updatedPerson);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+const notFoundMiddleware = (req, res, next) => {
+  res.status(404).send({ error: 'not found' });
+  next();
+};
+
+const errorMiddleware = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- models/person.js
+++ models/person.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then((result) => {
+  .then(() => {
     console.log('connected to MongoDB');
   })
   .catch((error) => {
@@ -12,8 +12,21 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+    },
+  },
 });
 
 personSchema.set('toJSON', {
